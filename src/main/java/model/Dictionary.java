62aece171922346e8e64b09a77959798d4b9f67c
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-03-24 22:32:44 +0700

    add code dictionary:

--- src/main/java/model/Dictionary.java
+++ src/main/java/model/Dictionary.java
@@ -14,6 +14,68 @@ public class Dictionary {
     private String mean;
     private String example;
     private boolean status = false;
+
+    public Dictionary(String english, String type, String mean, String example) {
+        this.english = english;
+        this.type = type;
+        this.mean = mean;
+        this.example = example;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMean() {
+        return mean;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setMean(String mean) {
+        this.mean = mean;
+    }
+
+    public void setExample(String example) {
+        this.example = example;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Dictionary{" + "english=" + english + ", type=" + type + ", mean=" + mean + ", example=" + example + ", status=" + status + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
     
    
 }
